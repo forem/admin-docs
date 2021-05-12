908d71341b25fc27875f58e67091602f4f8d1579
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2021-05-12 19:02:46 -0400

    Use Netlify custom URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://forem-admin.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
